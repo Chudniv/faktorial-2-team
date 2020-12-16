3810b3bfb2b251bfb805288c036ec18c2ca46cd7
yurko12 <yurkotv@gmail.com>
2020-12-16 10:41:04 +0000

    Написав функцію

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
-long long factorial(int number){
-	return 0;
+long double fact(int N)
+{
+    if(N < 0) // если пользователь ввел отрицательное число
+        return 0; // возвращаем ноль
+    if (N == 0) // если пользователь ввел ноль,
+        return 1; // возвращаем факториал от нуля - не удивляетесь, но это 1 =)
+    else // Во всех остальных случаях
+        return N * fact(N - 1); // делаем рекурсию.
 }
 
 int main(){
 	int number;
 	cin>>number;
-	cout<<factorial(number);
+	cout<<fact(number);
 	return 0;
 }
